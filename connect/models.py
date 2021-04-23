8b84fca4871b558438efaaf3214f1a1d7ad7e647
alfrtruj <alfrtruj@protonmail.com>
2021-04-23 22:29:38 +0200

    add PostgreSQL DB and created model

---
+++ connect/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Files(models.Model):
+    thumbnail = models.ImageField(upload_to='uploads/images', null=False, blank=False)
+    filename = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField()
+    package = models.CharField(max_length=100, null=False, blank=False)
+    size = models.CharField(max_length=10, null=False, blank=False)
+    date = models.DateField(default=timezone.now)
+    filetype = models.CharField(max_length=50, null=False, blank=False)
+
+
+    def __str__(self):
+        return self.filename
